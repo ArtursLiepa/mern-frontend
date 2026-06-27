b13fb05bd05ab178f621c661703c04d97aa0abb4
Arturs Liepa <arturs.liepa85@gmail.com>
2026-06-27 04:12:13 +0300

    Debug API URL

--- src/components/LayoutComponent/LayoutComponent.js
+++ src/components/LayoutComponent/LayoutComponent.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 const LayoutComponent = () => {
   const [information, setInformation] = useState({});
   const url = process.env.REACT_APP_API_URL;
+  console.log("FULL API URL:", JSON.stringify(url));
   // const url = "http://localhost:3005";
 
   const getInformationTitle = () => {
